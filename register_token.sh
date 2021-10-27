d825798ddc613c282188be020d2af9c95165bbb4
Stephanie M. Gogarten <sdmorris@uw.edu>
2021-10-26 22:16:04 -0700

    replace echo with printf

--- register_token.sh
+++ register_token.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 
-echo -n "-  hostname: https://imputation.biodatacatalyst.nhlbi.nih.gov
-   token: " > ~/.imputationbot/imputationbot.instances
-
+mkdir ~/.imputationbot
+printf -- "-  hostname: https://imputation.biodatacatalyst.nhlbi.nih.gov\n   token: " > ~/.imputationbot/imputationbot.instances
 cat $1 >> ~/.imputationbot/imputationbot.instances
 
 imputationbot instances
